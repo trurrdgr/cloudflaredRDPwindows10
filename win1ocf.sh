1da1059d5a93b4a95f7e3c5971eb7891c3ec1ab2
trurrdgr <100024417+trurrdgr@users.noreply.github.com>
2023-07-17 12:56:56 +0530

    Update win1ocf.sh

--- win1ocf.sh
+++ win1ocf.sh
@@ -5,15 +5,16 @@ wget -O cloudflared-linux-amd64 https://github.com/cloudflare/cloudflared/releas
 chmod +x cloudflared-linux-amd64
 
 # Run Cloudflare tunnel
-./cloudflared-linux-amd64 tunnel --url http://localhost:4000 &>/dev/null &
+./cloudflared-linux-amd64 tunnel --url http://localhost:8080 --label test >cf.txt
 
 # Run Docker container with NoMachine
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
 
+clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo "Done! NoMachine Information:"
 echo "IP Address:"
-./cloudflared-linux-amd64 tunnel list | awk '/^ID/{getline; print $2}'
+./cloudflared-linux-amd64 tunnel list | grep -oP '(?<=URL: http:\/\/).*$'
 echo "User: user"
 echo "Passwd: 123456"
 echo "VM can't connect? Restart Cloud Shell then re-run the script."
