876908a031b7656bbfb3aa326310fbc580355503
trurrdgr <100024417+trurrdgr@users.noreply.github.com>
2023-07-17 13:11:44 +0530

    Update win1ocf.sh

--- win1ocf.sh
+++ win1ocf.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+function goto {
+    label=$1
+    cd
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 |
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
 # Download and set up cloudflared
-wget -O cloudflared-linux-amd64 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+wget -O cloudflared-linux-amd64 https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 > /dev/null 2>&1
 chmod +x cloudflared-linux-amd64
 
-# Run Cloudflare tunnel
-./cloudflared-linux-amd64 tunnel --url http://localhost:8080 --label test >cf.txt
+# Run cloudflared tunnel
+clear
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "Choose Cloudflare tunnel region (for better connection)."
+echo "======================="
+echo "europe-west1 - Europe West (Frankfurt)"
+echo "us-east1 - US East (South Carolina)"
+echo "ap-northeast1 - Asia Pacific (Tokyo)"
+echo "us-west1 - US West (San Francisco)"
+read -p "Choose Cloudflare tunnel region: " CRP
+./cloudflared-linux-amd64 tunnel --region $CRP --url http://localhost:4040 &> cf.txt &
+sleep 1
+
+# Check if Cloudflare tunnel is running
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Cloudflare Tunnel Error! Please try again!" && sleep 1 && goto cloudflared; fi
 
 # Run Docker container with NoMachine
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
@@ -14,7 +37,7 @@ clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo "Done! NoMachine Information:"
 echo "IP Address:"
-./cloudflared-linux-amd64 tunnel list | grep -oP '(?<=URL: http:\/\/).*$'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"http:..([^"]*).*/\1/p'
 echo "User: user"
 echo "Passwd: 123456"
 echo "VM can't connect? Restart Cloud Shell then re-run the script."
